1ebe34a0fa548cda5b6dca986dd5db269a9fb10e
swerner1511 <s.werner_1990@gmx.net>
2018-01-02 18:02:36 +0100

    Update 02_de-install.sh

--- 02_de-install.sh
+++ 02_de-install.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+# Created by S. W.
+# latest update 02.01.2018
+#
+# Destop Enviroment installation (Gnome)
+
+echo "###################"
+echo "# gnome+extras... #"
+echo "###################"
+pacman -S --needed --noconfirm gnome gnome-extra
+systemctl enable gdm.service
+systemctl enable NetworkManager.service
+pacman -S --needed --noconfirm system-config-printer
+echo "#"
+echo "###########################"
+echo "# remove unwanted pkgs... #"
+echo "###########################"
+pacman -R --noconfirm baobab empathy epiphany totem accerciser aisleriot anjuta atomix five-or-more four-in-a-row gnome-2048 gnome-chess gnome-klotski gnome-mahjongg gnome-mines gnome-music gnome-nibbles gnome-robots gnome-sudoku gnome-taquin gnome-tetravex hitori iagno lightsoff orca quadrapassel swell-foop tali
