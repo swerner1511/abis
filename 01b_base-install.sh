cb34f6a1b04bd51e1a749a86a0dd2b08c6188bef
swerner1511 <s.werner_1990@gmx.net>
2018-01-02 18:42:51 +0100

    Update 01b_base-install.sh

--- 01b_base-install.sh
+++ 01b_base-install.sh
@@ -49,3 +49,88 @@ echo "# Set console keymap #"
 echo "######################"
 echo "#setting console keymap..."
 echo "KEYMAP=de-latin1-nodeadkeys" > /etc/vconsole.conf
+echo "#"
+echo "#########################"
+echo "# Set Hooks for ramdisk #"
+echo "#########################"
+echo "#Add scripts to ramdisk..."
+sed -i '/HOOKS="base udev autodetect modconf block filesystems keyboard fsck"/c\HOOKS="base udev autodetect modconf keyboard keymap block encrypt lvm2 filesystems fsck"' /etc/mkinitcpio.conf
+echo "#"
+echo "###################"
+echo "# Refresh ramdisk #"
+echo "###################"
+echo "#Regenerate ramdisk..."
+mkinitcpio -p linux
+echo "#"
+echo "###########################"
+echo "# Grub install and config #"
+echo "###########################"
+echo "#Install Grub..."
+pacman -S --needed --noconfirm grub
+echo "#"
+echo "#Configuring Grub..."
+# TODO : shorten the UUID-Part ^^
+# grep only UUID
+cryptsetup luksDump /dev/sda2 | grep UUID > tempUUID.txt
+# remove "UUID:" from string
+sed -ie 's/UUID:/ /g' tempUUID.txt
+# remove spaces and tabs from string
+sed -ie 's/^[ \t]*//' tempUUID.txt
+# $UUIDtemp = Inhalt von tempUUID.txt
+TempUUID=$(<tempUUID.txt)
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="cryptdevice=UUID='"${TempUUID}"':crypt0 root=/dev/mapper/vg0-root"/g' /etc/default/grub
+echo "#"
+echo "#Cleaning up..."
+rm ~/tempUUID.txt
+echo "#"
+echo "#Install grub to disk..."
+grub-install /dev/sda
+echo "#"
+echo "#Generate Grub config..."
+grub-mkconfig -o /boot/grub/grub.cfg
+echo "#"
+echo "##################"
+echo "# xorg server... #"
+echo "##################"
+pacman -S --needed --noconfirm xorg-server xorg-xinit
+localectl --no-convert set-x11-keymap de pc105 nodeadkeys
+echo "#"
+echo "######################"
+echo "# some basic pkgs... #"
+echo "######################"
+pacman -S --needed --noconfirm acpid avahi cups cronie firefox firefox-i18n-de thunderbird thunderbird-i18n-de
+systemctl enable acpid
+systemctl enable avahi-daemon
+systemctl enable cronie
+systemctl enable org.cups.cupsd.service
+ntpdate -u 0.de.pool.ntp.org
+hwclock -w
+echo "#"
+echo "#######################"
+echo "# graphical driver... #"
+echo "#######################"
+pacman -S --needed --noconfirm xf86-video-intel
+sh ./02_de-install.sh
+
+
+echo "#"
+echo "###########################"
+echo "# create useraccount... #"
+echo "###########################"
+echo "Enter your username..."
+read USERNAME
+useradd -m -g users -G wheel,audio,video -s /bin/bash $USERNAME
+passwd $USERNAME
+echo "User created..."
+echo "Don't forget to EDITOR=nano visudo..."
+echo "Press any key..."
+read
+EDITOR=nano visudo
+echo ""
+echo "###########################"
+echo "# set root password... #"
+echo "###########################"
+passwd
+echo "#"
+echo "#Exit from chroot environment..."
+exit
